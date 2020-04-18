339a34f12fbc308d93f6bb081f820b8d68dc0ab3
David Garza <dgarza0413@gmail.com>
2020-04-17 19:12:55 -0500

    resolvers and index inclue datasources w- apis

--- server/datasources/user.js
+++ server/datasources/user.js
@@ -1,112 +1,83 @@
-const UserAPI = require('../user');
+const { DataSource } = require('apollo-datasource');
+const isEmail = require('isemail');
 
-const mockStore = {
-    users: {
-        findOrCreate: jest.fn(),
-        findAll: jest.fn(),
-    },
-    trips: {
-        findOrCreate: jest.fn(),
-        destroy: jest.fn(),
-        findAll: jest.fn(),
-    },
-};
-module.exports.mockStore = mockStore;
+class UserAPI extends DataSource {
+    constructor({ store }) {
+        super();
+        this.store = store;
+    }
 
-const ds = new UserAPI({ store: mockStore });
-ds.initialize({ context: { user: { id: 1, email: 'a@a.a' } } });
+    /**
+     * This is a function that gets called by ApolloServer when being setup.
+     * This function gets called with the datasource config including things
+     * like caches and context. We'll assign this.context to the request context
+     * here, so we can know about the user making requests
+     */
+    initialize(config) {
+        this.context = config.context;
+    }
 
-describe('[UserAPI.findOrCreateUser]', () => {
-    it('returns null for invalid emails', async () => {
-        const res = await ds.findOrCreateUser({ email: 'boo!' });
-        expect(res).toEqual(null);
-    });
+    /**
+     * User can be called with an argument that includes email, but it doesn't
+     * have to be. If the user is already on the context, it will use that user
+     * instead
+     */
+    async findOrCreateUser({ email: emailArg } = {}) {
+        const email =
+            this.context && this.context.user ? this.context.user.email : emailArg;
+        if (!email || !isEmail.validate(email)) return null;
 
-    it('looks up/creates user in store', async () => {
-        mockStore.users.findOrCreate.mockReturnValueOnce([{ id: 1 }]);
+        const users = await this.store.users.findOrCreate({ where: { email } });
+        return users && users[0] ? users[0] : null;
+    }
 
-        // check the result of the fn
-        const res = await ds.findOrCreateUser({ email: 'a@a.a' });
-        expect(res).toEqual({ id: 1 });
+    async bookTrips({ launchIds }) {
+        const userId = this.context.user.id;
+        if (!userId) return;
 
-        // make sure store is called properly
-        expect(mockStore.users.findOrCreate).toBeCalledWith({
-            where: { email: 'a@a.a' },
+        let results = [];
+
+        // for each launch id, try to book the trip and add it to the results array
+        // if successful
+        for (const launchId of launchIds) {
+            const res = await this.bookTrip({ launchId });
+            if (res) results.push(res);
+        }
+
+        return results;
+    }
+
+    async bookTrip({ launchId }) {
+        const userId = this.context.user.id;
+        const res = await this.store.trips.findOrCreate({
+            where: { userId, launchId },
         });
-    });
+        return res && res.length ? res[0].get() : false;
+    }
 
-    it('returns null if no user found/created', async () => {
-        // store lookup is not mocked to return anything, so this
-        // simulates a failed lookup
+    async cancelTrip({ launchId }) {
+        const userId = this.context.user.id;
+        return !!this.store.trips.destroy({ where: { userId, launchId } });
+    }
 
-        const res = await ds.findOrCreateUser({ email: 'a@a.a' });
-        expect(res).toEqual(null);
-    });
-});
-
-describe('[UserAPI.bookTrip]', () => {
-    it('calls store creator and returns result', async () => {
-        mockStore.trips.findOrCreate.mockReturnValueOnce([{ get: () => 'heya' }]);
-
-        // check the result of the fn
-        const res = await ds.bookTrip({ launchId: 1 });
-        expect(res).toBeTruthy();
-
-        // make sure store is called properly
-        expect(mockStore.trips.findOrCreate).toBeCalledWith({
-            where: { launchId: 1, userId: 1 },
+    async getLaunchIdsByUser() {
+        const userId = this.context.user.id;
+        const found = await this.store.trips.findAll({
+            where: { userId },
         });
-    });
-});
+        return found && found.length
+            ? found.map(l => l.dataValues.launchId).filter(l => !!l)
+            : [];
+    }
 
-describe('[UserAPI.bookTrips]', () => {
-    it('returns multiple lookups from bookTrip', async () => {
-        mockStore.trips.findOrCreate.mockReturnValueOnce([{ get: () => 'heya' }]);
-        mockStore.trips.findOrCreate.mockReturnValueOnce([{ get: () => 'okay' }]);
+    async isBookedOnLaunch({ launchId }) {
+        if (!this.context || !this.context.user) return false;
+        const userId = this.context.user.id;
+        const found = await this.store.trips.findAll({
+            where: { userId, launchId },
+        });
+        return found && found.length > 0;
+    }
+}
 
-        const res = await ds.bookTrips({ launchIds: [1, 2] });
-        expect(res).toEqual(['heya', 'okay']);
-    });
-});
-
-describe('[UserAPI.cancelTrip]', () => {
-    it('calls store destroy and returns result', async () => {
-        const args = { userId: 1, launchId: 1 };
-        mockStore.trips.destroy.mockReturnValueOnce('heya');
-
-        // check the result of the fn
-        const res = await ds.cancelTrip(args);
-        expect(res).toEqual(true);
-
-        // make sure store is called properly
-        expect(mockStore.trips.destroy).toBeCalledWith({ where: args });
-    });
-});
-
-describe('[UserAPI.getLaunchIdsByUser]', () => {
-    it('looks up launches by user', async () => {
-        const args = { userId: 1 };
-        const launches = [
-            { dataValues: { launchId: 1 } },
-            { dataValues: { launchId: 2 } },
-        ];
-        mockStore.trips.findAll.mockReturnValueOnce(launches);
-
-        // check the result of the fn
-        const res = await ds.getLaunchIdsByUser(args);
-        expect(res).toEqual([1, 2]);
-
-        // make sure store is called properly
-        expect(mockStore.trips.findAll).toBeCalledWith({ where: args });
-    });
-
-    it('returns empty array if nothing found', async () => {
-        const args = { userId: 1 };
-        // store lookup is not mocked to return anything, so this
-        // simulates a failed lookup
-
-        // check the result of the fn
-        const res = await ds.getLaunchIdsByUser(args);
-        expect(res).toEqual([]);
-    });
-});
+module.exports = UserAPI;
--- server/index.js
+++ server/index.js
@@ -5,7 +5,23 @@ const path = require('path');
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, 'utf-8');
 
-const server = new ApolloServer({ typeDefs })
+const { createStore } = require('./utils')
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const resolvers = require('./resolvers');
+const store = createStore();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+    })
+
+})
 
 server.listen().then(({ url }) =>
     console.log(`server ready at ${url}`))
