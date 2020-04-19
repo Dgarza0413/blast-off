1b6b6022240061a45cdcbfc9a45d001889ca3065
David Garza <dgarza0413@gmail.com>
2020-04-19 17:27:21 -0500

    added mutations mission launch and user resolvers

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, 'utf-8');
+const isEmail = require('isemail');
 
 const { createStore } = require('./utils')
 
@@ -14,13 +15,22 @@ const resolvers = require('./resolvers');
 const store = createStore();
 
 const server = new ApolloServer({
+    context: async ({ req }) => {
+        const auth = req.headers && req.headers.authorization || '';
+        const email = Buffer.from(auth, 'base64').toString('ascii');
+        if (!isEmail.validate(email)) return { user: null };
+        // find a user by their email
+        const users = await store.users.findOrCreate({ where: { email } });
+        const user = users && users[0] || null;
+
+        return { user: { ...user.dataValues } };
+    },
     typeDefs,
     resolvers,
     dataSources: () => ({
         launchAPI: new LaunchAPI(),
         userAPI: new UserAPI({ store })
-    })
-
+    }),
 })
 
 server.listen().then(({ url }) =>
