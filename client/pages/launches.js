204b36f839c80a8d9c4b581de8b2a1acda0eb4da
David Garza <dgarza0413@gmail.com>
2020-04-20 12:36:00 -0500

    added assets and components to client

--- client/pages/launches.js
+++ client/pages/launches.js
@@ -6,14 +6,64 @@ import { LaunchTile, Header, Button, Loading } from '../components';
 
 const GET_LAUNCHES = gql`
     query launchList($after: String){
-        
+        launches(after: $after){
+            cursor
+            hasMore
+            launches {
+                id
+                isBooked
+                rocket {
+                    id
+                    name
+                }
+                mission {
+                    name
+                    missionPatch
+                }
+            }
+        }
     }
 `
 
-export default function launches() {
-    return (
-        <div>
+const launches = () => {
+    const { data, loading, error, fetchMore } = useQuery(GET_LAUNCHES);
 
-        </div>
-    )
-}
+    if (loading) return <Loading />;
+    if (error) return <p>ERROR</p>;
+    if (!data) return <p>Not found</p>;
+
+    return (
+        <>
+            <Header />
+            {data.launches &&
+                data.launches.hasMore && (
+                    <Button onClick={() =>
+                        fetchMore({
+                            variables: {
+                                after: data.launches.cursor,
+                            },
+                            updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                                if (!fetchMoreResult) return prev;
+                                return {
+                                    ...fetchMoreResult,
+                                    launches: {
+                                        ...fetchMoreResult.launches,
+                                        launches: [
+                                            ...prev.launches.launches,
+                                            ...fetchMoreResult.launches.launches,
+                                        ],
+                                    },
+                                };
+                            },
+                        })
+                    }
+                    >
+                        Load More
+                    </Button>
+                )
+            }
+        </>
+    );
+};
+
+export default launches;
